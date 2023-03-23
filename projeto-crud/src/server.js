8946a2fa85d117a32fed7b3c9f992b165afbd158
Marcelo Tadini <tadini.marcelo53@gmail.com>
2023-03-23 16:24:43 -0300

    adding routes and connecting database

--- projeto-crud/src/server.js
+++ projeto-crud/src/server.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const path = require('path')
+const db = require('./database')
+const routes = require('./routes')
 
 const app = express()
+
+//conexão com o banco de dados
+db.connect()
 
 //definindo o template engine
 app.set('view engine', 'ejs')
@@ -13,12 +18,8 @@ app.use(express.static(path.join(__dirname, 'public')))
 //habilita o server para receber dados via post (formulário)
 app.use(express.urlencoded({extended: true}))
 
-//rotas
-app.get("/", (req,res) => {
-    res.render('index', {
-        title: 'Titulo Teste'
-    })
-})
+//definindo as rotas
+app.use('/', routes)
 
 //404 error (not found)
 app.use((req, res) => { //middleware
